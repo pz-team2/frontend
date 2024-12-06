ef703ef13624aab726e354a219d68d4ec44672e1
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2024-12-06 14:39:23 +0700

    private-route

--- src/Logout.tsx
+++ src/Logout.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react"
+import React from "react"
 
 export const Logout = () => {
 
     useEffect(() => {
         localStorage.removeItem('token')
         localStorage.removeItem('username')
+        localStorage.removeItem('role')
         window.location.href = '/page'
     }, [])
   return (
