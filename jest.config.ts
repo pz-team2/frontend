054a249fa49e7f46d632a13ba642323ef813fee9
Chiefff67 <bocahrkr@gmail.com>
2024-12-14 03:28:48 +0800

    fixbug jest test

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,12 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.app.json" }],
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.app.json" }], // TypeScript transform
+    "^.+\\.jsx?$": "babel-jest", // Babel transform untuk file JS/JSX
   },
+  moduleNameMapper: {
+  "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/test/__mocks__/fileMock.js",  // <--- perhatikan perubahan di sini
+},
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
