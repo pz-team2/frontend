a12a348afd9d25ef0755f52e459dc851c922c93a
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2024-12-13 17:49:25 +0700

    api

--- src/admin/pages/event/UpdateEvent.tsx
+++ src/admin/pages/event/UpdateEvent.tsx
@@ -1,7 +1,7 @@
 import { AiFillPlusCircle } from "react-icons/ai";
 import React from 'react'
-import { IoChevronBackOutline } from "react-icons/io5";
-import { Link, useNavigate, useParams } from 'react-router-dom';
+// import { IoChevronBackOutline } from "react-icons/io5";
+import { useNavigate, useParams } from 'react-router-dom';
 import { Input } from '../../../components/Fragments/Input';
 import { Editor as TinyMCEEditor } from '@tinymce/tinymce-react';
 import { useAppDispatch } from "../../../Redux/hook";
