c88fa4f5a8b1f2c43b45d9b56828e3ffb2aafb9b
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2024-12-14 18:36:48 +0700

    coverage

--- jest.config.ts
+++ jest.config.ts
@@ -17,10 +17,6 @@ module.exports = {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.app.json" }], // TypeScript transform
     "^.+\\.jsx?$": "babel-jest", // Babel transform untuk file JS/JSX
   },
-  moduleNameMapper: {
-  "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/test/__mocks__/fileMock.js",  // <--- perhatikan perubahan di sini
-},
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   extensionsToTreatAsEsm: [".ts", ".tsx"], // Jika TypeScript digunakan
   globals: {
